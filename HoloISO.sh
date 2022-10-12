b9dd3e058bd9cfda4bda9ecd7b6bc8013a1977cb
Potts <ianpottinger@gmail.com>
2022-10-12 18:06:15 +0100

    Update HoloISO.sh

--- HoloISO.sh
+++ HoloISO.sh
@@ -1,1 +1,6 @@
 sudo mkdir -p /lib/firmware/edid && echo "AP///////wAJ5QMAAwAAAAEdAQOACxF4LwAAoFdJmyYQSE8AAAABAQEBAQEBAQEBAQEBAQEBwhogUDAAEFAQEDIAbKwAAAAYAAAA/ABUVjA4MFdVTS1OTDAKAAAA/QA8PBAQBwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMM=" | base64 --decode | sudo tee /lib/firmware/edid/gpdwinmax.bin
+
+pacman -S extra/mesa multilib/lib32-mesa holo/gamescope
+
+echo "IgnorePkg = gamescope mesa lib32-mesa" | sudo tee /etc/pacman.conf
+
